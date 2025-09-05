e4194b4c27a1831d34b8e8a2ff2fe03da1507447
BreackNeackPeack <133713653+BreackNeackPeack@users.noreply.github.com>
2025-09-05 14:05:30 -0300

    Foram feitas atualizações nas classes MenuUsuario e ScreenMatchApplication.
    No MenuUsuario, Foi criado uma lista de dados para as temporadas da série, em seguida, um "for", que vai de 1 até a última temporada (se houver), e que atribuirá o ENDEREÇO (fixo do OMDB), + o buscaSerie (o nome da série que foi digitado + a API_Key, seguido por um conversor e um forEach, que exibe cada um.
    
    E no ScreenMatchApplication foi alterado somente a forma de chamar a função MenuUsuario.

--- src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.ScreenMatch;
 
-import br.com.alura.ScreenMatch.model.DadosEpisodio;
-import br.com.alura.ScreenMatch.model.DadosSerie;
-import br.com.alura.ScreenMatch.model.DadosTemporada;
-import br.com.alura.ScreenMatch.services.ConsumoAPI;
-import br.com.alura.ScreenMatch.services.ConverteDados;
+import br.com.alura.ScreenMatch.principal.MenuUsuario;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenMatchApplication implements CommandLineRunner {
@@ -21,23 +14,11 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.devolverDados("https://www.omdbapi.com/?t=Squid+Game&apikey=dd43a8e1");
-		System.out.println(json );
+		MenuUsuario menuUsuario = new MenuUsuario();
+		menuUsuario.exibirMenu();
 
-		ConverteDados converteDados = new ConverteDados();
-		DadosSerie dadosSerie = converteDados.obterdados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-		json = consumoAPI.devolverDados("https://www.omdbapi.com/?t=Squid+Game&season=1&episode=1&apikey=dd43a8e1");
-		DadosEpisodio dadosEpisodio = converteDados.obterdados(json,DadosEpisodio.class );
-		System.out.println(dadosEpisodio);
-		List<DadosTemporada> temporadas = new ArrayList<>();
-		for (int i = 1; i <= dadosSerie.TotalTemporadas(); i++){
-			json = consumoAPI.devolverDados("https://www.omdbapi.com/?t=Squid+Game&season=" + i + "&apikey=dd43a8e1");
-			DadosTemporada dadosTemporada = converteDados.obterdados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
 
-		}
-		temporadas.forEach(System.out::println);
 	}
 }
+
+
--- src/main/java/br/com/alura/ScreenMatch/principal/MenuUsuario.java
+++ src/main/java/br/com/alura/ScreenMatch/principal/MenuUsuario.java
@@ -1,11 +1,13 @@
 package br.com.alura.ScreenMatch.principal;
 
+import br.com.alura.ScreenMatch.model.DadosEpisodio;
 import br.com.alura.ScreenMatch.model.DadosSerie;
+import br.com.alura.ScreenMatch.model.DadosTemporada;
 import br.com.alura.ScreenMatch.services.ConsumoAPI;
 import br.com.alura.ScreenMatch.services.ConverteDados;
 import com.sun.jdi.PrimitiveValue;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class MenuUsuario {
     //Variáveis:
@@ -27,6 +29,24 @@ public class MenuUsuario {
         var consumoAPI = new ConsumoAPI();
         var json = consumoAPI.devolverDados(ENDERECO + buscaSerie.replace(" ", "+") + API_KEY);
         DadosSerie dadosSerie = converteDados.obterdados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
 
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dadosSerie.TotalTemporadas(); i++){
+            json = consumoAPI.devolverDados(ENDERECO + buscaSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = converteDados.obterdados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+        temporadas.forEach(System.out::println);
+        /*for (int i = 0; i < dadosSerie.TotalTemporadas(); i++){
+            List<DadosEpisodio> dadosEpisodios = temporadas.get(i).episodios();
+            for(int j = 0; j< dadosEpisodios.size(); j++){
+                System.out.println(dadosEpisodios.get(j).Titulo());*/
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.Titulo())));
+
+            {
+        }
     }
 }
